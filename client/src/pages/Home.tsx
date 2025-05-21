c3d07e9d9969462cedf5bf13695fd9ce3bc8e102
NabilY-5 <nabilyahya59280@gmail.com>
2025-05-21 16:41:33 +0200

    feat: work on homepage and route created

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import "../styles/home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Bienvenue sur Père Castor Quiz !</h1>
+      <p>
+        Apprends le développement en t'amusant avec nos quiz pour toute la
+        famille.
+      </p>
+
+      <div className="dev-story">
+        <h2>Le développement, c'est quoi ?</h2>
+        <p>
+          Le développement, c'est comme créer des jeux ou des histoires avec un
+          ordinateur. <br /> On utilise des "langages secrets" (comme JavaScript
+          ou Python) pour dire à la machine quoi faire.
+          <br /> C'est un peu comme apprendre une nouvelle langue, mais pour
+          parler avec ton ordinateur !
+        </p>
+      </div>
+      <p className="quiz-choice">
+        Prêts maintenant ? Choisis une thématique et lance-toi dans l'aventure
+      </p>
+
+      <div className="quiz-topics">
+        <div className="quiz-thematique">HTML et CSS</div>
+        <div className="quiz-thematique">Python</div>
+        <div className="quiz-thematique">JavaScript</div>
+        <div className="quiz-thematique">React</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
