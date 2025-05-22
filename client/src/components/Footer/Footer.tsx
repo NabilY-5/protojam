dc9ab22cbe7fd615f83cbee53533485b54b21f3b
corentinthebaud <corentin.thebaud44@gmail.com>
2025-05-22 09:10:33 +0200

    feat: Footer done

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li className="site-info">
+          <h3>Liens utiles</h3>
+          <section className="logos">
+            <a href="https://developer.mozilla.org/fr/" target="blank">
+              <img src="/logo/mdn-logo.png" alt="logo MDN" />
+            </a>
+            <a href="https://fr.react.dev/" target="blank">
+              <img src="/logo/react.svg" alt="logo React" />
+            </a>
+          </section>
+        </li>
+
+        <li className="mentions-légales">
+          <p>© Copyright</p>
+          <p>Site développé par Wild Dev</p>
+          <p>Mentions légales</p>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
